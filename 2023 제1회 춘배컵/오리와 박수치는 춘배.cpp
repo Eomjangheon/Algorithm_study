bc03adda95aafa3d00b20cbead99cab4db086833
Eomjangheon <mpmpm0610@gmail.com>
2023-11-14 23:42:45 +0900

    제1회 춘배컵 : 오리와 박수치는 춘배
    그리디 알고리즘

---
+++ 2023 제1회 춘배컵/오리와 박수치는 춘배.cpp
@@ -0,0 +1,34 @@
+/*
+2023 제1회 춘배컵
+2번 문제
+문제이름	: 무지개 만들기 
+문제번호	: 30403
+난이도	: b2
+소요시간	: ??
+*/
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+
+using namespace std;
+
+void algoInit()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+}
+
+
+
+int main()
+{
+	algoInit();
+	int64_t n;
+	cin >> n;
+	for (int64_t i = 0; i < n; ++i) {
+		cout << "SciComLove\n";
+	}
+}
