9fc40ebd060d95933b8e289bdf41c6d131ed5631
Eomjangheon <mpmpm0610@gmail.com>
2025-03-23 23:32:50 +0900

    인프런 : 1주차 J문제

--- 인프런/1주차/J_9375_패션왕신해빈.cpp
+++ 인프런/1주차/J_9375_패션왕신해빈.cpp
@@ -7,32 +7,34 @@
 
 using namespace std;
 
-map<int, string> intToStr;
-map<string, int> strToInt;
+
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	int n, m;
-	cin >> n >> m;
-	for (int i = 1; i <= n; ++i) {
-		string str;
-		cin >> str;
-		intToStr[i] = str;
-		strToInt[str] = i;
-	}
+	int n;
+	cin >> n;
 
-	while (m--) {
-		string str;
-		cin >> str;
-		if (isalpha(str[0])) {
-			cout << strToInt[str] << "\n";
+	while (n--) {
+		map<string, int> mp;
+		int m;
+		cin >> m;
+		while (m--) {
+			string name, type;
+			cin >> name >> type;
+			mp[type]++;
 		}
-		else {
-			cout << intToStr[stoi(str)] << "\n";
+
+		int sum = 1;
+		for (auto a : mp) {
+			sum *= (a.second + 1);
 		}
+		sum--;
+		cout << sum << "\n";
+
 	}
 
 }
